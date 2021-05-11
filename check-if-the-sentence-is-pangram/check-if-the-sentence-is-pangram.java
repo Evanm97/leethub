91852665707370a76701f63088fc32aab97be70e
Evan <evanmurray2012@gmail.com>
2021-05-12 00:12:28 +0100

    Update check-if-the-sentence-is-pangram.java

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
+        
         if(sentence.length() < 26) {
             return false;
         }
-        
+
         boolean pangram = false;
         ArrayList<Character> array = new ArrayList<Character>();
         int count = 0;
 
-       for(int i = 0; i<sentence.length(); i++){
-           if(!array.contains(sentence.charAt(i))) {
-               array.add(sentence.charAt(i));
-               count ++;
-           }
-           
-       }
-    return pangram = count > 25;
+        for(int i = 0; i<sentence.length(); i++){
+            if(!array.contains(sentence.charAt(i))) {
+                array.add(sentence.charAt(i));
+                count ++;
+            }
+        }
+        return pangram = count > 25;
     }
 }
